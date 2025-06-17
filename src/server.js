a017f8ae056c0c61bc580ca7d0bb5ea348b0b059
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-06-17 20:24:52 -0300

    aprendendo sobre scripts e deixando as atualizações do codigo em tempo real

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 
 const server = http.createServer ((request, response) => {
-    return response.end('ooi')
+    return response.end('ooie')
 })
 
 server.listen(3333) //localhost
