b604f2250f4df4119663fd331c95c0c8ecda39de
MurilloAzevedo <murilloazevedo2@gmail.com>
2025-06-18 10:00:34 -0300

    Utilização de GET e POST

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 import http from 'node:http'
 
+const user = []  // Iremos converter esse array em JSON para enviar ao frontend
+
 const server = http.createServer ((request, response) => {
     const {method, url} = request
 
     if(method === 'GET' && url === '/users'){
-        return response.end('Listagem de usuarios')
+        return response
+        .setHeader('Content-Type', 'application/json') // Definindo o tipo de conteúdo da resposta como JSON
+        .end(JSON.stringify(user)) // resposta de um node para o frontend não pode ser um array, tem que ser uma string ou objeto JSON
     }
 
     if(method === 'POST' && url === '/users'){
+
+        user.push({
+            id: 1,
+            name: 'teste',
+            email: 'teste@example.com',
+        })
+
         return response.end('Criação de usuario')
     }
 
