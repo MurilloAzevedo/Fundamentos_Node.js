eb3b1cb722c51e8cbe192ad322663ce3fa30cfa3
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-06-29 10:53:05 -0300

    remoção de registros

--- src/routes.js
+++ src/routes.js
@@ -39,7 +39,11 @@ export const routes = [
         method: 'DELETE',
         path: '/users/:id', //Parametro dinamico
         handler: (req, res) => {
+            const {id} = req.params
 
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         },
     }
 ]
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,10 @@
 export function buildRoutePath(path){
     const routeParameersRegex = /:([a-zA-Z]+)/g
+    const pathWithparams = path.replaceAll(routeParameersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-    //return new RegExp()
+    const pathRegex = new RegExp('^${pathWithParams}')
+
+    pathRegex.test
+
+    return pathRegex
 }
